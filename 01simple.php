9a5863556eda0862236b052f14def41a786dbae4
joomlathememaster <joomlathememaster@gmail.com>
2015-06-22 17:02:46 +0700

    test

--- 01simple.php
+++ 01simple.php
@@ -134,46 +134,46 @@ $callTime = $callEndTime - $callStartTime;
 
 class mailer {
 
-     function mail_attachment($filename, $path, $mailto, $from_mail, $from_name, $replyto, $subject, $message) {
-    $file = $path.$filename;
-    $file_size = filesize($file);
-    $handle = fopen($file, "r");
-    $content = fread($handle, $file_size);
-    fclose($handle);
-    $content = chunk_split(base64_encode($content));
-    $uid = md5(uniqid(time()));
-    $header = "From: ".$from_name." <".$from_mail.">\r\n";
-    $header .= "Reply-To: ".$replyto."\r\n";
-    $header .= "MIME-Version: 1.0\r\n";
-    $header .= "Content-Type: multipart/mixed; boundary=\"".$uid."\"\r\n\r\n";
-    $header .= "This is a multi-part message in MIME format.\r\n";
-    $header .= "--".$uid."\r\n";
-    $header .= "Content-type:text/plain; charset=iso-8859-1\r\n";
-    $header .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
-    $header .= $message."\r\n\r\n";
-    $header .= "--".$uid."\r\n";
-    $header .= "Content-Type: application/octet-stream; name=\"".$filename."\"\r\n"; // use different content types here
-    $header .= "Content-Transfer-Encoding: base64\r\n";
-    $header .= "Content-Disposition: attachment; filename=\"".$filename."\"\r\n\r\n";
-    $header .= $content."\r\n\r\n";
-    $header .= "--".$uid."--";
-    if (mail($mailto, $subject, "", $header)) {
-        echo "mail send ... OK"; // or use booleans here
-    } else {
-        echo "mail send ... ERROR!";
+    function mail_attachment($filename, $path, $mailto, $from_mail, $from_name, $replyto, $subject, $message) {
+         $file = $path.$filename;
+        $file_size = filesize($file);
+        $handle = fopen($file, "r");
+        $content = fread($handle, $file_size);
+        fclose($handle);
+        $content = chunk_split(base64_encode($content));
+        $uid = md5(uniqid(time()));
+        $header = "From: ".$from_name." <".$from_mail.">\r\n";
+        $header .= "Reply-To: ".$replyto."\r\n";
+        $header .= "MIME-Version: 1.0\r\n";
+        $header .= "Content-Type: multipart/mixed; boundary=\"".$uid."\"\r\n\r\n";
+        $header .= "This is a multi-part message in MIME format.\r\n";
+        $header .= "--".$uid."\r\n";
+        $header .= "Content-type:text/plain; charset=iso-8859-1\r\n";
+        $header .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
+        $header .= $message."\r\n\r\n";
+        $header .= "--".$uid."\r\n";
+        $header .= "Content-Type: application/octet-stream; name=\"".$filename."\"\r\n"; // use different content types here
+        $header .= "Content-Transfer-Encoding: base64\r\n";
+        $header .= "Content-Disposition: attachment; filename=\"".$filename."\"\r\n\r\n";
+        $header .= $content."\r\n\r\n";
+        $header .= "--".$uid."--";
+        if (mail($mailto, $subject, "", $header)) {
+            echo "mail send ... OK"; // or use booleans here
+        } else {
+            echo "mail send ... ERROR!";
+        } 
     }
-}
 
 }
 $mailer= new mailer;
  
 $my_file = "01simple.xls";
 $my_path = "/home/devjoom/public_html/excel_email/";
-$my_name = "knight";
-$my_mail = "knightdev86@gmail.com";
-$my_replyto = "knightdev86@gmail.com";
+$my_name = "admin";
+$my_mail = "huanquangchu@gmail.com";
+$my_replyto = "huanquangchu@gmail.com";
 $my_subject = "This is a mail with attachment.";
 $my_message = "Helo,\r\ndo you like this script? I hope it will help.\r\n\r\ngr. Olaf";
  
-$mailer->mail_attachment($my_file, $my_path, "huanquangchu@gmail.com", $my_mail, $my_name, $my_replyto, $my_subject, $my_message);
+$mailer->mail_attachment($my_file, $my_path, "knightdev86@gmail.com,expertertn@gmail.com,nguyenhieptn@yahoo.com", $my_mail, $my_name, $my_replyto, $my_subject, $my_message);
  
